f99e468bb0daf4c214fddc169a9733de99a72957
Liam Fruzyna <liam@fruzyna.net>
2025-09-20 21:07:02 -0500

    Package customization

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-### Install packages
+# add WildRank dependencies
+dnf5 install -y \
+    python3-fastapi-slim+standard \
+    python3-python-multipart \
+    python3-requests \
+    python3-uvicorn+standard
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# add other desired packages
+dnf5 install -y \
+    firefox \
+    gnome-shell-extension-drive-menu \
+    java-21-openjdk-devel \
+    keepassxc
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# remove default gnome extensions
+dnf5 remove -y \
+    gnome-shell-extension-blur-my-shell \
+    gnome-shell-extension-caffeine \
+    gnome-shell-extension-dash-to-dock \
+    gnome-shell-extension-gsconnect \
+    gnome-shell-extension-logo-menu \
+    gnome-shell-extension-tailscale-gnome-qs
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# remove unwanted packages
+dnf5 remove -y \
+    tailscale \
+    ublue-brew \
+    ublue-motd
 
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+cp /ctx/post-install.sh /
+chmod +x /post-install.sh
